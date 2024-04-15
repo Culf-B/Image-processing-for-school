c51c336fe3d208fe185509b2d969ebaddfae7ac9
Culf-B <49363745+Culf-B@users.noreply.github.com>
2024-04-15 08:49:49 +0200

    Set pixel function

--- sketch.js
+++ sketch.js
@@ -6,8 +6,17 @@ function draw() {
   background(220);
 }
 
-function mySetPixel() {
+function mySetPixel(x, y, pic, r, g, b, a) {
+  pic.loadPixels();
 
+  pixelStartIndex = (x + pic.width * y) * 4;
+
+  pic.pixels[pixelStartIndex] = r;
+  pic.pixels[pixelStartIndex + 1] = g;
+  pic.pixels[pixelStartIndex + 2] = b;
+  pic.pixels[pixelStartIndex + 3] = a;
+
+  pic.updatePixels();
 }
 
 function myGetPixel() {
